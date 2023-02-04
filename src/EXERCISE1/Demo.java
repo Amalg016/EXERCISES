71f454b310a9c3ec831c14260cd7599430169624
Amalg016 <amalgjoseph1@gmail.com>
2023-02-04 18:33:39 +0530

    4

--- src/EXERCISE1/Demo.java
+++ src/EXERCISE1/Demo.java
@@ -4,10 +4,10 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] d= {"s","d"};
         Browser b=new Browser();
         b.visit("Sss");
         b.getUrls();
+        String[] d= {"s","d"};
         Browser a=new Browser(d);
         a.getUrls();
 	}
